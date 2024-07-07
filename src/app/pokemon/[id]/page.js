9fe3d17c1aaaba6e1338cd8a7eac340cc279f156
Elton Costa <89019551+ryzxxn@users.noreply.github.com>
2024-07-07 17:07:28 +0530

    asdasd

--- src/app/pokemon/[id]/page.js
+++ src/app/pokemon/[id]/page.js
@@ -1,118 +1,188 @@
-'use client'
-import React, { useState, useEffect } from 'react';
+'use client';
+import { useParams } from 'next/navigation';
+import axios from 'axios';
+import { useState, useEffect, useCallback, useMemo } from 'react';
+import StatBar from '@/app/components/stat_bar';
 import Image from 'next/image';
-import Statbar from '@/app/Components/statbar';
-import { MdOutlineHeight } from "react-icons/md";
-import { FaWeightScale } from "react-icons/fa6";
+import { motion } from 'framer-motion'
+import { IoMdArrowDropdown } from "react-icons/io";
 
-export default function Page({ params }) {
-  const { id } = params;
-  const [pokemonData, setPokemonData] = useState(null);
-  const [paddedID, setpaddedID] = useState(null)
+export default function PokemonPage() {
+  const { id } = useParams();
+  const [pokemon, setPokemon] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [filteredMoves, setFilteredMoves] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [evolutionChain, setEvolutionChain] = useState(null);
+
+  const fetchPokemonData = useCallback(async () => {
+    try {
+      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+      const pokemonData = response.data;
+      const paddedId = pokemonData.id.toString().padStart(3, '0');
+      pokemonData.image = `https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/${paddedId}.png`;
+      setPokemon(pokemonData);
+      setLoading(false);
+    } catch (error) {
+      setError(error);
+      setLoading(false);
+    }
+  }, [id]);
 
   useEffect(() => {
-    const fetchData = async () => {
+    fetchPokemonData();
+  }, [fetchPokemonData]);
+
+  useEffect(() => {
+    if (pokemon) {
+      const filtered = pokemon.moves.filter((move) =>
+        move.move.name.includes(searchTerm.toLowerCase())
+      );
+      setFilteredMoves(filtered.slice(0, 8));
+    }
+  }, [pokemon, searchTerm]);
+
+  useEffect(() => {
+    const fetchEvolutionChain = async () => {
       try {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        setPokemonData(data);
-        setpaddedID(data.id.toString().padStart(3, '0'))
+        const response = await axios.get(
+          `https://pokeapi.co/api/v2/pokemon-species/${pokemon?.id}`
+        );
+        const speciesUrl = response.data.evolution_chain.url;
+        const evolutionChainResponse = await axios.get(speciesUrl);
+        setEvolutionChain(evolutionChainResponse.data.chain);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error(error);
       }
     };
 
-    fetchData();
-  }, [id]);
+    if (pokemon) {
+      fetchEvolutionChain();
+    }
+  }, [pokemon]);
 
-  if (!pokemonData) {
-    return <></>
-  }
+  const typeColors = useMemo(() => ({
+    normal: '#A8A878',
+    fire: '#F08030',
+    water: '#6890F0',
+    grass: '#78C850',
+    flying: '#A890F0',
+    fighting: '#C03028',
+    poison: '#A040A0',
+    electric: '#F8D030',
+    ground: '#E0C068',
+    rock: '#B8A038',
+    psychic: '#F85888',
+    ice: '#98D8D8',
+    bug: '#A8B820',
+    ghost: '#705898',
+    steel: '#B8B8D0',
+    dragon: '#7038F8',
+    dark: '#705848',
+    fairy: '#F0B6BC',
+  }), []);
+
+  const renderEvolutionChain = (chain) => {
+    if (!chain) return null;
+
+    return (
+      <div style={{display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center'}}>
+        <p className='capitalize'>{chain.species.name}</p>
+        <IoMdArrowDropdown />
+        {chain.evolves_to.map((evolution) => (
+          <div key={evolution.species.name}>
+            <p className='capitalize' style={{textAlign: 'center'}}>
+              {evolution.evolution_details[0].trigger.name} at level{' '}
+              {evolution.evolution_details[0].min_level}
+            </p>
+            {renderEvolutionChain(evolution)}
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  if (loading) return <div></div>;
+  if (error) return <div>Error: {error.message}</div>;
 
   return (
-    <div className=' bg-black flex flex-col h-screen sm:flex-col md:flex-row lg:flex-row'>
-      <div className='h-full flex justify-center'>
-        {pokemonData && (
-            <div className='flex flex-col w-full min-w-28'>
-              <div className='flex justify-between' style={{padding: "0rem 2rem"}}>
-                  <h1 className='capitalize text-white text-left text-[2rem]'>{pokemonData.name}</h1>
-                  <h1 className='capitalize text-gray-500 text-left text-[2rem] font-mono'>#{paddedID}</h1>
-              </div>
-
-              <div className='w-full flex flex-col text-white' style={{padding: "0rem 2rem"}}>
-                <p className='flex items-center text-[.9rem] gap-4'><MdOutlineHeight className='text-[1.2rem]'/>{pokemonData.height}0 cm</p>
-                <p className='flex items-center text-[.9rem] gap-4'><FaWeightScale className='text-[1.2rem]'/>{pokemonData.weight} lbs</p>
-              </div>
-
-              <div className="w-full h-auto justify-center flex">
-                  <Image
-                      src={pokemonData.sprites.other['official-artwork'].front_default}
-                      alt={`Image of ${pokemonData.name}`}
-                      height={500}
-                      width={500}
-                      priority
-                      className='w-[15rem]'
-                  />
-              </div>
-              <div className='w-full flex flex-col' style={{padding: "0rem 2rem"}}>
-                  {pokemonData.stats.map((item, index) => (
-                      <div key={index} className='flex flex-col'>
-                          <div className='text-white capitalize flex justify-between items-center'>
-                            <p className='text-[.9rem]'>{item.stat.name}</p>
-                            <p className='text-[.8rem]'>{item.base_stat}</p>
-                          </div>
-                          <Statbar key={index} percent={item.base_stat} />
-                      </div>
-                  ))}
-              </div>
-              <div className='w-full flex flex-col gap-0  text-white' style={{padding: "1rem 2rem"}}>
-              <h1>TYPE</h1>
-                <div className='flex gap-4'>
-                {pokemonData.types.map((item, index) => (
-                  <div key={index} className='flex flex-row'>
-                    <div className='text-gray-500 capitalize flex justify-between items-center'>
-                      <p className='text-[.9rem]'>{item.type.name}</p>
-                    </div>
-                  </div>
-                ))}
-                </div>
-              </div>
-
-              <div className='w-full flex flex-col text-white' style={{padding: "0rem 2rem"}}>
-              <h1>ABILITY</h1>
-                  {pokemonData.abilities.map((item, index) => (
-                      <div key={index} className='flex flex-col'>
-                          <div className='text-gray-500 capitalize flex justify-between items-center'>
-                          <p className='text-[.9rem]'>{item.ability.name}</p>
-
-                          </div>
-                      </div>
-                  ))}
-              </div>
-
-
-            </div>
-        )}
-      </div>
-      <div className='bg-yellow-400 h-full flex flex-1 overflow-scroll'>
-      <div className='w-full flex-col text-white' style={{padding: "0rem 2rem"}}>
-          <h1>MOVES</h1>
-          <div className='z-10'>
-            <div className='flex flex-col overflow-scroll gap-4 text-nowrap'>
-                {pokemonData.moves.map((item, index) => (
-                  <div key={index} className='flex flex-col'>
-                    <div className='text-gray-500 capitalize flex justify-between items-center'>
-                      <p className='text-[.9rem] p-2 rounded-lg z-0' style={{border: '1px solid gray'}}>{item.move.name}</p>
-                    </div>
-                  </div>
-                ))}
-            </div>
+    <>
+      <div style={{ display: 'flex', width: '100%', padding: '0rem 2rem' }} className='flex-col md:flex-row'>
+        <div key={pokemon.id} style={{ display: 'flex', flex: '1', flexDirection: 'column' }}>
+          <h1 style={{ color: 'white', fontSize: '2rem' }}>{pokemon.name.toUpperCase()}</h1>
+          <div style={{ display: 'flex', gap: '1rem' }}>
+            {pokemon.types.map((type) => (
+              <p
+                className="capitalize"
+                key={type.type.name}
+                style={{ display: 'inline-block', color: typeColors[type.type.name] }}
+              >
+                {type.type.name}
+              </p>
+            ))}
           </div>
+          <p style={{ color: 'white' }}>Weight: {pokemon.weight} lbs</p>
+          <p style={{ color: 'white' }}>Height: {pokemon.height}0 cm</p>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+          >
+            <Image
+              src={pokemon.image} // Path to your image
+              alt="Example Image"
+              objectFit=''
+              quality={0}
+              width={500} // Desired width of the image
+              height={500} // Desired height of the image
+              placeholder='blur'
+              blurDataURL='/images/pokeball.png'
+              style={{objectFit: 'contain'}}
+            />
+          </motion.div>
+        </div>
+
+        <div style={{ display: 'flex', flex: '1', flexDirection: 'column', padding: '0rem 0rem', justifyContent: 'center', gap: '1rem' , alignItems: 'center'}}>
+          <div style={{width: '100%', display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
+            <h1 style={{ color: 'white', fontSize: '2rem' }}>Stats</h1>
+            {pokemon.stats.map((stat) => (
+              <div key={stat.stat.name} style={{ color: 'white', width:'100%', }}>
+                {stat.stat.name.toUpperCase()}: {stat.base_stat}
+                <StatBar percent={(stat.base_stat / 255) * 100} />
+              </div>
+            ))}
+          </div>
+
+<div style={{display: 'flex', justifyContent: 'space-between', flex: '1'}}>
+          <div style={{ display: 'flex', flexDirection: 'column', gap: '0rem', padding: '0rem 0rem', flex: '1', width: 'max-content' }}>
+            <h1 style={{ color: 'white', fontSize: '2rem' }}>Moves</h1>
+            <input
+              type="text"
+              placeholder="Search moves..."
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              style={{
+                backgroundColor: 'transparent',
+                color: 'white',
+              }}
+            />
+            <ul style={{ listStyle: 'none', padding: 0, margin: 0 }}>
+              {filteredMoves.map((move) => (
+                <li className='capitalize' key={move.move.name} style={{ color: 'white' }}>
+                  {move.move.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div style={{ color: 'white', flex: '1'}}>
+            <h1 style={{ color: 'white', fontSize: '2rem' }}>Evolution</h1>
+            {renderEvolutionChain(evolutionChain)}
+          </div>
+</div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
